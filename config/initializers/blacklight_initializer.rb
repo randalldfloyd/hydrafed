b931943ab45370317290e7d062cc57e446b510f5
Randall Floyd <rdfloyd@indiana.edu>
2015-05-12 15:28:08 -0400

    Adding a plain Hydra app to play with federated content in Fedora 4

---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = '6860bb153d49623cac8b6c132c177d00919c35729dadbb26b3cebc759fc76a979ac457250f576e701cf06ce02e878229e75abf69c60dcd895fa9d525363661ec'
+
